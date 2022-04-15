d3ccb4da3e09c55e1b8103b3eca3c6bd5538ac86
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-04-15 15:40:57 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WonderFruit from './WonderFruit';
+
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<WonderFruit />, rootElement);
