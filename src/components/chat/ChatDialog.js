eb76c6a066af83c144f77b54d75c11e3ed8aa2e0
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-04-15 17:12:05 +0900

    Update ChatDialog.js

--- src/components/chat/ChatDialog.js
+++ src/components/chat/ChatDialog.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import SearchBar from '../widgets/SearchBar';
 
 const ChatDialog = (props) => {
 
+    const searchPerson = () => {
+        console.log("search person entered !!")
+    }
+
+    const openSearchDialog = () => {
+        console.log("search clicked !!")
+    }
+
+    return (
+        <div style={{ height: '600px', width: "100%" }}>
+            <SearchBar 
+                doSearchCallback={ searchPerson } 
+                label="이름, 전화번호 또는 담당업무로 검색"
+                onClick={ openSearchDialog }
+            />
+        </div>
+    );
 }
 
 export default ChatDialog;
